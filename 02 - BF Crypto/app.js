7abbcfa82cc27210639a0cd2713e996af481cc85
Pedro Henrique <phc.contato22@gmail.com>
2022-03-02 20:30:49 -0300

    BF Crypto

--- 02 - BF Crypto/app.js
+++ 02 - BF Crypto/app.js
@@ -55,7 +55,6 @@ if(animate.length){
 
 
 //Propriedade personagens
-
 //guerreiros
 const guerComum = document.getElementById('guerreiro1');
 
@@ -112,6 +111,107 @@ guerLendario.addEventListener("mouseout", function(){
 
 //Arqueiros
 const arqComum = document.getElementById('arqueiro1');
+
+arqComum.addEventListener("mouseover", function(){
+    arqComum.src = "./images/propriedade_arqueiro1.png";
+    arqComum.style.cursor = "pointer";
+})
+
+arqComum.addEventListener("mouseout", function(){
+    arqComum.src = "./images/arqueiro1.png";
+})
+
+
+const arqRaro = document.getElementById('arqueiro2');
+
+arqRaro.addEventListener("mouseover", function(){
+    arqRaro.src = "./images/propriedade_arqueiro2.png";
+    arqRaro.style.cursor = "pointer"
+})
+
+arqRaro.addEventListener("mouseout", function(){
+    arqRaro.src = "./images/arqueiro2.png";
+})
+
+
+const arqEpico = document.getElementById("arqueiro3");
+
+arqEpico.addEventListener("mouseover", function(){
+    arqEpico.src = "./images/propriedade_arqueiro3.png";
+    arqEpico.style.cursor = "pointer";
+})
+
+arqEpico.addEventListener("mouseout", function(){
+    arqEpico.src = "./images/arqueiro3.png";
+    arqEpico.style.cursor = "pointer";
+})
+
+
+const arqLendario = document.getElementById('arqueiro4');
+
+arqLendario.addEventListener("mouseover", function(){
+    arqLendario.src = "./images/propriedade_arqueiro4.png";
+    arqLendario.style.cursor = "pointer";
+})
+
+arqLendario.addEventListener("mouseout", function(){
+    arqLendario.src = "./images/arqueiro4.png";
+    arqLendario.style.cursor = "pointer";
+})
+
+
+//Magos
+
+const magoComum = document.getElementById('mago1');
+
+magoComum.addEventListener("mouseover", function(){
+    magoComum.src = "./images/propriedade_mago1.png";
+    magoComum.style.cursor = "pointer"
+})
+
+magoComum.addEventListener("mouseout", function(){
+    magoComum.src = "./images/mago1.png";
+    magoComum.style.cursor = "pointer";
+})
+
+
+const magoRaro = document.getElementById('mago2');
+
+magoRaro.addEventListener("mouseover", function(){
+    magoRaro.src = "./images/propriedade_mago2.png";
+    magoRaro.style.cursor = "pointer"
+})
+
+magoRaro.addEventListener("mouseout", function(){
+    magoRaro.src = "./images/mago2.png";
+    magoRaro.style.cursor = "pointer";
+})
+
+
+const magoEpico = document.getElementById('mago3');
+
+magoEpico.addEventListener("mouseover", function(){
+    magoEpico.src = "./images/propriedade_mago3.png";
+    magoEpico.style.cursor = "pointer";
+})
+
+magoEpico.addEventListener("mouseout", function(){
+    magoEpico.src = "./images/mago3.png";
+    magoEpico.style.cursor = "pointer";
+})
+
+
+const magoLendario = document.getElementById("mago4");
+
+magoLendario.addEventListener("mouseover", function(){
+    magoLendario.src = "./images/propriedade_mago4.png";
+    magoLendario.style.cursor = "pointer";
+})
+
+magoLendario.addEventListener("mouseout", function(){
+    magoLendario.src = "./images/mago4.png";
+    magoLendario.style.cursor = "pointer";
+})
 
 
 
