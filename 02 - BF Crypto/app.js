59dec62f144ee6efb4f7795de7adc2a7f25d5a9a
Pedro Henrique <phc.contato22@gmail.com>
2022-03-02 18:28:26 -0300

    BF Crypto

--- 02 - BF Crypto/app.js
+++ 02 - BF Crypto/app.js
@@ -54,6 +54,75 @@ if(animate.length){
 
 
 
+//Propriedade personagens
+
+//guerreiros
+const guerComum = document.getElementById('guerreiro1');
+
+guerComum.addEventListener("mouseover", function(){
+    guerComum.src = "./images/propriedade_guerreiro1.png";
+    guerComum.style.cursor = "pointer"
+})
+
+guerComum.addEventListener("mouseout", function(){
+    guerComum.src = "./images/guerreiro1.png";
+    guerComum.style.cursor = "pointer"
+})
+
+
+const guerRaro = document.getElementById('guerreiro2');
+
+guerRaro.addEventListener("mouseover", function(){
+    guerRaro.src = "./images/propriedade_guerreiro2.png";
+    guerRaro.style.cursor = "pointer"
+})
+
+guerRaro.addEventListener("mouseout", function(){
+    guerRaro.src = "./images/guerreiro2.png";
+    guerRaro.style.cursor = "pointer";
+})
+
+
+const guerEpico = document.getElementById('guerreiro3');
+
+guerEpico.addEventListener("mouseover", function(){
+    guerEpico.src = "./images/propriedade_guerreiro3.png";
+    guerEpico.style.cursor = "pointer";
+})
+
+guerEpico.addEventListener("mouseout", function(){
+    guerEpico.src = "./images/guerreiro3.png";
+    guerEpico.style.cursor = "pointer"
+})
+
+
+const guerLendario = document.getElementById('guerreiro4');
+
+guerLendario.addEventListener("mouseover", function(){
+    guerLendario.src = "./images/propriedade_guerreiro4.png";
+    guerLendario.style.cursor = "pointer";
+    
+})
+
+guerLendario.addEventListener("mouseout", function(){
+    guerLendario.src = "./images/guerreiro4.png";
+    guerLendario.style.cursor = "pointer";
+    
+})
+
+//Arqueiros
+const arqComum = document.getElementById('arqueiro1');
+
+
+
+
+
+
+
+
+
+
+
 
 
 
