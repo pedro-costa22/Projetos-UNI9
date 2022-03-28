208c66918169d7b46445f83e03c81f6969735c89
Pedro Henrique <phc.contato22@gmail.com>
2022-03-28 17:14:44 -0300

    página-de-proposta

--- 02 - BF Crypto/app.js
+++ 02 - BF Crypto/app.js
@@ -73,6 +73,41 @@ if(animate.length){
     window.addEventListener('scroll', animationScroll)
 }
 
+//--------------------- Slide ---------------------//
+
+let cont = 1;
+let id = document.querySelector("#radio1").checked = true;
+const btnNext = document.querySelector("#btn-next");
+const btnBack = document.querySelector("#btn-back");
+
+setInterval(() =>{
+   nextSlide()
+}, 10000)
+
+function nextSlide(){
+    cont++;
+    
+    if(cont > 3) {
+        cont = 1;
+    }
+    id = document.querySelector("#radio" + cont).checked = true;
+}
+
+btnNext.addEventListener("click", nextSlide);
+
+btnBack.addEventListener("click",  () =>{
+    cont--;
+
+    if(cont > 3) {
+        cont = 1;
+
+    } if (cont < 1) {
+        cont = 3;
+    }
+
+    id = document.querySelector("#radio" + cont).checked = true;
+})
+
 
 
 //--------------------- Clique p/ ver propriedades ---------------------// 
